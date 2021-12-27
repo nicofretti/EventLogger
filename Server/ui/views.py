4aa9cd4f8c6b1b74224e7459ff31b979d2cdbfca
Frex <nico.fretti@gmail.com>
2021-12-27 19:00:15 +0100

    [Server] Setup templates

--- Server/ui/views.py
+++ Server/ui/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+
 
 # Create your views here.
+def login(request):
+    username = request.POST['username']
+    password = request.POST['password']
+    user = authenticate(request, username=username, password=password)
+
+    if user is not None:
+        # Logged
+        pass
+    else:
+        # Invalid login
+        pass
