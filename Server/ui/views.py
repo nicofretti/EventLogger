4e5e0c95933b01da51360869d1031de2858009f0
Frex <nico.fretti@gmail.com>
2022-01-05 15:27:40 +0100

    Process listing

--- Server/ui/views.py
+++ Server/ui/views.py
@@ -6,6 +6,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login as auth_login, logout as auth_logout
 import api.models as models
 import json
+import random
 
 
 def login(request):
@@ -43,10 +44,11 @@ def homepage(request):
 
 def events(request, pk):
     events = models.Event.objects.filter(logger_key=pk)
+    assigned_colors = {}
     custom_events = []
     for event in events:
         processes = json.loads(event.processes)
-        processes_string = get_processes_to_string(processes)
+        processes_string, assigned_colors = get_processes_to_string(processes, assigned_colors)
         event.processes = processes_string
         custom_events.append(event)
     context = {
@@ -58,19 +60,28 @@ def events(request, pk):
 
 # Useful methods
 
-def get_processes_to_string(processes):
+def get_processes_to_string(processes,assigned_colors):
     processes_custom = []
     for obj in processes:
         list_processes = ""
         for p in obj['list']:
-            list_processes += """<span class='process'>{} </span>""".format(p)
+            if(p in assigned_colors):
+                color = assigned_colors[p]
+            else:
+                random.shuffle(rainbow_colors)
+                color = rainbow_colors[random.randint(0, len(rainbow_colors) - 1)]
+                assigned_colors[p] = color
+            list_processes += """<span class='process'><span style='color:{}'>●</span>{}</span> """.format(color, p)
         obj['timestamp'] = datetime.datetime \
             .strptime(obj['timestamp'], '%d/%m/%Y %H:%M:%S') \
             .strftime('%H:%M:%S')
         obj['list'] = list_processes.strip(',')
         processes_custom.append(obj)
-    return processes_custom
+    return processes_custom, assigned_colors
 
-raimbow_colors = [
-    '#ff0000','#ffa500','#ffff00','#008000','#0000ff','#800080','#808080','#a52a2a','#ffc0cb','#00ffff','#ff00ff','#dc143c','#4b0082','#00ff00','#808000','#008080','#000080','#800000','#c0c0c0','#00ffff','#32cd32','#ffd700','#fa8072'
+
+rainbow_colors = [
+    '#ff0000', '#ffa500', '#ffff00', '#008000', '#0000ff', '#800080', '#808080', '#a52a2a', '#ffc0cb', '#00ffff',
+    '#ff00ff', '#dc143c', '#4b0082', '#00ff00', '#808000', '#008080', '#000080', '#800000', '#c0c0c0', '#00ffff',
+    '#32cd32', '#ffd700', '#fa8072 '
 ]
